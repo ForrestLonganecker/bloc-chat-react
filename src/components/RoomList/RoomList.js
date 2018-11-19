e34806a489926e32a8dbf71ae74490097c248811
Forrest Longanecker <ForrLong.Tech@gmail.com>
2018-11-19 05:53:24 -0800

    checkpoint 3 complete

--- src/components/RoomList/RoomList.js
+++ src/components/RoomList/RoomList.js
@@ -31,8 +31,7 @@ class RoomList extends Component {
     handleSubmit(e) {
         e.preventDefault();
         if (!this.state.newRoomName) { return }
-
-        const newRoom = this.state.newRoomName;
+        const newRoom = { name: this.state.newRoomName };
         this.roomsRef.push({ name:newRoom });
         this.setState({ rooms: [...this.state.rooms, newRoom], newRoomName: ''});
     }
